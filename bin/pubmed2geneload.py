032f20f8e8455c2a2841a5cb54885a295ce8374a
Sharon Cousins Giannatto <sharon.giannatto@jax.org>
2020-04-27 11:55:53 -0400

    tabs expanded

--- bin/pubmed2geneload.py
+++ bin/pubmed2geneload.py
@@ -1,4 +1,3 @@
-#!/usr/local/bin/python
 
 ##########################################################################
 #
@@ -47,7 +46,7 @@ import string
 import db
 import loadlib
 import runCommand
-	
+        
 TAB = '\t'
 CRT = '\n'
 
@@ -171,111 +170,111 @@ def init():
     # -- use to determine if pm ID associated w/reference that has a JNum
     # -- used in report of multi egIDs assoc with pmid in file
     results = db.sql('''select a1._Object_key as refsKey, a1.accid as mgiID,
-	     a3.accid as pmID
-	from ACC_Accession a1, ACC_Accession a2, ACC_Accession a3
-	where a1._MGIType_key = 1
-	and a1._LogicalDB_key = 1
-	and a1.preferred = 1
-	and a1.prefixPart = 'MGI:'
-	and a1._Object_key = a2._Object_key
+             a3.accid as pmID
+        from ACC_Accession a1, ACC_Accession a2, ACC_Accession a3
+        where a1._MGIType_key = 1
+        and a1._LogicalDB_key = 1
+        and a1.preferred = 1
+        and a1.prefixPart = 'MGI:'
+        and a1._Object_key = a2._Object_key
         and a2._MGIType_key = 1
         and a2._LogicalDB_key = 1
         and a2.prefixPart = 'J:'
         and a2.preferred = 1
-	and a1._Object_key = a3._Object_key
-	and a3._MGIType_key = 1
-	and a3._LogicalDB_key = 29
-	and a3.preferred = 1''', 'auto')
+        and a1._Object_key = a3._Object_key
+        and a3._MGIType_key = 1
+        and a3._LogicalDB_key = 29
+        and a3.preferred = 1''', 'auto')
     for r in results:
-	pmID = r['pmID']
-	mgiID = r['mgiID']
-	refsKey = r['refsKey']
-	if pmID not in dbPmToMgiDict:
-	    dbPmToMgiDict[pmID] = []
-	dbPmToMgiDict[pmID].append([mgiID, refsKey])
+        pmID = r['pmID']
+        mgiID = r['mgiID']
+        refsKey = r['refsKey']
+        if pmID not in dbPmToMgiDict:
+            dbPmToMgiDict[pmID] = []
+        dbPmToMgiDict[pmID].append([mgiID, refsKey])
     # -- get mouse gene/ref pairs
     # -- load into dict, use to iterate through pubmed/id gene pairs
     # -- and report genes assoc with >1 pmid at EG
     results = db.sql('''select geneid, pubmedid
-	from DP_EntrezGene_PubMed
-	where taxid = 10090''', 'auto')
+        from DP_EntrezGene_PubMed
+        where taxid = 10090''', 'auto')
     totalAssocInput = len(results)
     for r in results:
-	pmID = r['pubmedid']
-	egID = r['geneid']
- 	#if egID not in inputEgToPmDict:
-	#    inputEgToPmDict[egID] = []
-	#inputEgToPmDict[egID].append(pmID)
-	if pmID  not in inputPmToEgDict:
-	    inputPmToEgDict[pmID] = []
-	inputPmToEgDict[pmID].append(egID)
-	
+        pmID = r['pubmedid']
+        egID = r['geneid']
+        #if egID not in inputEgToPmDict:
+        #    inputEgToPmDict[egID] = []
+        #inputEgToPmDict[egID].append(pmID)
+        if pmID  not in inputPmToEgDict:
+            inputPmToEgDict[pmID] = []
+        inputPmToEgDict[pmID].append(egID)
+        
 
     # -- use to determine if egID assoc >1 marker
     # -- report EG ID and gene mgiID and marker symbol
     results = db.sql('''select a1.accid as egID, m._Marker_key, m.symbol, 
-	    a2.accid as markerID
-	from ACC_Accession a1, MRK_Marker m, ACC_Accession a2
-	where a1._MGIType_key = 2
-	and a1._LogicalDB_key = 55
-	and a1.preferred = 1
-	and a1._Object_key = m._Marker_key
-	and a1._Object_key = a2._Object_key
-	and a2._MGIType_key = 2
-	and a2._LogicalDB_key = 1
-	and a2.preferred = 1
-	and a2.prefixPart = 'MGI:'
-	order by a1.accid''', 'auto')
+            a2.accid as markerID
+        from ACC_Accession a1, MRK_Marker m, ACC_Accession a2
+        where a1._MGIType_key = 2
+        and a1._LogicalDB_key = 55
+        and a1.preferred = 1
+        and a1._Object_key = m._Marker_key
+        and a1._Object_key = a2._Object_key
+        and a2._MGIType_key = 2
+        and a2._LogicalDB_key = 1
+        and a2.preferred = 1
+        and a2.prefixPart = 'MGI:'
+        order by a1.accid''', 'auto')
     for r in results:
-	egID = r['egID']
-	markerID = r['markerID']
-	symbol = r['symbol']
-	markerKey = r['_Marker_key']
+        egID = r['egID']
+        markerID = r['markerID']
+        symbol = r['symbol']
+        markerKey = r['_Marker_key']
         if egID not in dbEgToMarkerDict:
-	    dbEgToMarkerDict[egID] = []
-	listToAppend = [markerID, symbol, markerKey]
-	if listToAppend not in dbEgToMarkerDict[egID]:
-	    dbEgToMarkerDict[egID].append(listToAppend)
+            dbEgToMarkerDict[egID] = []
+        listToAppend = [markerID, symbol, markerKey]
+        if listToAppend not in dbEgToMarkerDict[egID]:
+            dbEgToMarkerDict[egID].append(listToAppend)
     # -- used to determine if there is an existing ref/egid curated
     # -- association in the database
     # -- 1571 - exclude this loads user key as it hasn't been deleted yet.
     results = db.sql('''select distinct ra._Refs_key, a._Object_key as markerKey, a.accid as egId
-	from MGI_Reference_Assoc ra, ACC_Accession a, MGI_User u
-	where ra._MGIType_key = 2
-	and ra._refassoctype_key = 1018
-	and ra._CreatedBy_key != 1571
-	and ra._CreatedBy_key = u._User_key
-	and ra._Object_key = a._Object_key 
-	and a._MGIType_key = 2
-	and a._LogicalDB_key = 55 --entrezgene
-	and a.preferred = 1''', 'auto')
+        from MGI_Reference_Assoc ra, ACC_Accession a, MGI_User u
+        where ra._MGIType_key = 2
+        and ra._refassoctype_key = 1018
+        and ra._CreatedBy_key != 1571
+        and ra._CreatedBy_key = u._User_key
+        and ra._Object_key = a._Object_key 
+        and a._MGIType_key = 2
+        and a._LogicalDB_key = 55 --entrezgene
+        and a.preferred = 1''', 'auto')
 
     for r in results:
-	refsKey = r['_Refs_key']
-	egID = r['egId']
-	markerKey = r['markerKey']
-	if refsKey not in curRefDict:
-	    curRefDict[refsKey] = []
-	curRefDict[refsKey].append(markerKey)
+        refsKey = r['_Refs_key']
+        egID = r['egId']
+        markerKey = r['markerKey']
+        if refsKey not in curRefDict:
+            curRefDict[refsKey] = []
+        curRefDict[refsKey].append(markerKey)
 
     # -- get status of all refs in the database
     # -- used to update GO status when refs associated with genes
     db.sql('''select _Refs_key, _Status_key
-	into temporary table goCurrent
-	from BIB_Workflow_Status
-	where _Group_key = 31576666
-	and isCurrent = 1''', None)
+        into temporary table goCurrent
+        from BIB_Workflow_Status
+        where _Group_key = 31576666
+        and isCurrent = 1''', None)
     db.sql('create index idx2 on goCurrent(_Refs_key)', None)
     results = db.sql('''select a.mgiid as refID, b._Refs_key, b.isDiscard, 
-	    s._Status_key
+            s._Status_key
         from BIB_Refs b, goCurrent s, BIB_Citation_Cache a
         where b._Refs_key = s._Refs_key
         and b._Refs_key = a._Refs_key''', 'auto')
     for r in results:
-	refID = r['refID']
-	isDiscard = r['isDiscard']
-	statusKey = r['_Status_key']
-	dbRefIdToStatusDict[refID] = [isDiscard, statusKey]
+        refID = r['refID']
+        isDiscard = r['isDiscard']
+        statusKey = r['_Status_key']
+        dbRefIdToStatusDict[refID] = [isDiscard, statusKey]
     return 0
 
 #
@@ -292,54 +291,54 @@ def createBCP():
     global totalAdded, totalDeleted, refList, refMarkerList
         
     for pmID in inputPmToEgDict:
-	egList = inputPmToEgDict[pmID]
+        egList = inputPmToEgDict[pmID]
 
-	if len(egList) > 15: # 2
-	    inputPmIdMultiEgList.append('%s%s%s' % (pmID, TAB, string.join(egList)))
-	    continue
-	#egID = egList[0]  # we know we have < 16
-	if pmID not in dbPmToMgiDict: # 1
-	    inputPmIdNotInMgiList.append(pmID)
-	    continue
+        if len(egList) > 15: # 2
+            inputPmIdMultiEgList.append('%s%s%s' % (pmID, TAB, string.join(egList)))
+            continue
+        #egID = egList[0]  # we know we have < 16
+        if pmID not in dbPmToMgiDict: # 1
+            inputPmIdNotInMgiList.append(pmID)
+            continue
         if len(dbPmToMgiDict[pmID]) > 1:
             # Just curious so checking  - this will go to diag log
             print 'more than one reference object for %s in database %s' % (pmID, dbPmToMgiDict[pmID])
             continue
 
-	# get the reference info first
-	refInfo = dbPmToMgiDict[pmID][0] # Here we now we have one as multi's
+        # get the reference info first
+        refInfo = dbPmToMgiDict[pmID][0] # Here we now we have one as multi's
                                          # reported above
                                          # [mgiID, refsKey]
         refID  = refInfo[0]
         refKey = refInfo[1]
 
-	# now iterate over the genes for this reference
-	for egID in egList:
-	    # egID not in MGI?, report
-	    if egID not in dbEgToMarkerDict: # 3
-		inputEgIdNotInMgiList.append('%s%s%s' % (egID, TAB, pmID))
-		continue
-	    # egID associated with > marker in MGI?, report
-	    if len(dbEgToMarkerDict[egID]) > 1: # 4
-		markerList = dbEgToMarkerDict[egID]
-		reportList = []
-		for l in markerList:
-		    mID = l[0]
-		    symbol = l[1]
-		    reportList.append('%s|%s' % (mID, symbol))
-		egIdMultiGenesList.append('%s%s%s%s%s' % \
-		    (egID, TAB, pmID, TAB, string.join(reportList)))
-		continue
+        # now iterate over the genes for this reference
+        for egID in egList:
+            # egID not in MGI?, report
+            if egID not in dbEgToMarkerDict: # 3
+                inputEgIdNotInMgiList.append('%s%s%s' % (egID, TAB, pmID))
+                continue
+            # egID associated with > marker in MGI?, report
+            if len(dbEgToMarkerDict[egID]) > 1: # 4
+                markerList = dbEgToMarkerDict[egID]
+                reportList = []
+                for l in markerList:
+                    mID = l[0]
+                    symbol = l[1]
+                    reportList.append('%s|%s' % (mID, symbol))
+                egIdMultiGenesList.append('%s%s%s%s%s' % \
+                    (egID, TAB, pmID, TAB, string.join(reportList)))
+                continue
 
-	    # get the marker  key
-	    markerInfo = dbEgToMarkerDict[egID][0] # Here we know we have one
-						   # as multi reported above
-	    
-	    markerKey = markerInfo[2]
-	    # skip if the reference/marker assoc already curated in the db
-	    if refKey in curRefDict and markerKey in curRefDict[refKey]:
-		totalAssocInDb += 1
-		continue
+            # get the marker  key
+            markerInfo = dbEgToMarkerDict[egID][0] # Here we know we have one
+                                                   # as multi reported above
+            
+            markerKey = markerInfo[2]
+            # skip if the reference/marker assoc already curated in the db
+            if refKey in curRefDict and markerKey in curRefDict[refKey]:
+                totalAssocInDb += 1
+                continue
 
             # skip if reference/marker is a duplicate
             if (refKey, markerKey) in refMarkerList:
@@ -347,12 +346,12 @@ def createBCP():
             else:
                 refMarkerList.append((refKey, markerKey))
 
-	    if refID not in refList:
-		refList.append(refID)
-	    totalAdded += 1
-	    fpBcp.write('%s|%s|%s|%s|%s|%s|%s|%s|%s\n' % \
-		   (refAssocKey, refKey, markerKey, mgiTypeKey, refAssocTypeKey,  createdByKey, createdByKey, loaddate, loaddate))
-	    refAssocKey = refAssocKey + 1
+            if refID not in refList:
+                refList.append(refID)
+            totalAdded += 1
+            fpBcp.write('%s|%s|%s|%s|%s|%s|%s|%s|%s\n' % \
+                   (refAssocKey, refKey, markerKey, mgiTypeKey, refAssocTypeKey,  createdByKey, createdByKey, loaddate, loaddate))
+            refAssocKey = refAssocKey + 1
     return 0
 
 #
@@ -366,29 +365,29 @@ def bcpFiles():
     global totalDeleted, refsDeletedList
     fpBcp.close()
     if DEBUG or not bcpon:
-	return 0
+        return 0
 
     db.commit()
 
     # delete from MGI_Reference_Assoc
     print 'deleting from MGI_Reference_Assoc'
     db.sql('''select _Assoc_key
-	into temporary table toDelete
-	from MGI_Reference_Assoc
-	where _CreatedBy_key = 1571''', None)
+        into temporary table toDelete
+        from MGI_Reference_Assoc
+        where _CreatedBy_key = 1571''', None)
 
    
     db.sql('''create index idx1 on toDelete(_Assoc_key)''', None)
 
     results = db.sql('''select count(*) as deleteCt
-	from toDelete''', 'auto')
+        from toDelete''', 'auto')
 
     for r in results:
-	totalDeleted = r['deleteCt']
+        totalDeleted = r['deleteCt']
 
     db.sql('''delete from MGI_Reference_Assoc a
-	using toDelete d
-	where a._Assoc_key = d._Assoc_key''', None)
+        using toDelete d
+        where a._Assoc_key = d._Assoc_key''', None)
 
     db.commit()
     print 'totalDeleted in bcpFiles(): %s' % totalDeleted
@@ -428,27 +427,27 @@ def updateGoStatus():
     # get list of refIDs that actually need updating, reporting as we go
     print 'updateGoStatus size of refList: %s' % len(refList)
     for refID in refList:
-	if refID in dbRefIdToStatusDict:
-	    print 'updateGoStatus: %s' % refID
-	    infoList = dbRefIdToStatusDict[refID]
-	    isDiscard = infoList[0]
-	    statusKey = infoList[1]
-	    print 'refID: %s, isDiscard: %s statusKey: %s' % (refID, isDiscard, statusKey)
-	    if isDiscard == 1 or statusKey == 31576672: # rejected
-		# report
-		d = 'False'
-		s = 'Not Rejected'
-		if isDiscard == 1:
-		    d = 'True'
-		if statusKey == 31576672:
-		    s = 'Rejected'
-		dOrRStatusList.append('%s%s%s%s%s' % (refID, TAB, d, TAB, s))
-		print 'writing %s to discardOrRejected report' % refID
-		continue
-	    if statusKey not in (31576673, 31576674): # Indexed, Full-coded
-		print 'adding %s to list to be updated' % refID
-		updateStatusList.append(refID)
-	else: # this should never happen
+        if refID in dbRefIdToStatusDict:
+            print 'updateGoStatus: %s' % refID
+            infoList = dbRefIdToStatusDict[refID]
+            isDiscard = infoList[0]
+            statusKey = infoList[1]
+            print 'refID: %s, isDiscard: %s statusKey: %s' % (refID, isDiscard, statusKey)
+            if isDiscard == 1 or statusKey == 31576672: # rejected
+                # report
+                d = 'False'
+                s = 'Not Rejected'
+                if isDiscard == 1:
+                    d = 'True'
+                if statusKey == 31576672:
+                    s = 'Rejected'
+                dOrRStatusList.append('%s%s%s%s%s' % (refID, TAB, d, TAB, s))
+                print 'writing %s to discardOrRejected report' % refID
+                continue
+            if statusKey not in (31576673, 31576674): # Indexed, Full-coded
+                print 'adding %s to list to be updated' % refID
+                updateStatusList.append(refID)
+        else: # this should never happen
             print 'RefID not in dbRefIdToStatusDict: %s' % refID
     # now do the updates
     numUpdates = len(updateStatusList)
@@ -456,14 +455,14 @@ def updateGoStatus():
     while updateStatusList != []:
         batchToRun = string.join(updateStatusList[0:UPDATE_BATCH], ',')
         #print batchToRun
-	del updateStatusList[0:UPDATE_BATCH]
-	print '%s' % mgi_utils.date()
-	print 'running runCommand'
-	print FULL_API_URL % batchToRun
-	stdout, stderr, returnCode = runCommand.runCommand(FULL_API_URL % batchToRun)
-	print 'after runCommand stdout: %s stderr: %s returnCode: %s' % (stdout, stderr, returnCode)
-	if returnCode != 0:
-	    return 1
+        del updateStatusList[0:UPDATE_BATCH]
+        print '%s' % mgi_utils.date()
+        print 'running runCommand'
+        print FULL_API_URL % batchToRun
+        stdout, stderr, returnCode = runCommand.runCommand(FULL_API_URL % batchToRun)
+        print 'after runCommand stdout: %s stderr: %s returnCode: %s' % (stdout, stderr, returnCode)
+        if returnCode != 0:
+            return 1
     return 0
 #
 # Purpose: write all discrepancies to curation log
@@ -479,33 +478,33 @@ def writeCuratorLog():
     fpLogCur.write(CRT + CRT + 'Total PubMed/EG ID added to the Database: %s' % totalAdded)
     fpLogCur.write(CRT + CRT + 'Total References  updated to "Indexed" for GO: %s' % numUpdates)
     if len(inputPmIdNotInMgiList):
-	fpLogCur.write(CRT + CRT + string.center(
-	    'PM IDs not in the Database or reference has no J: number',60) + CRT)
-	fpLogCur.write(string.join(inputPmIdNotInMgiList, CRT))
-	fpLogCur.write(CRT + 'Total: %s' % len(inputPmIdNotInMgiList))
+        fpLogCur.write(CRT + CRT + string.center(
+            'PM IDs not in the Database or reference has no J: number',60) + CRT)
+        fpLogCur.write(string.join(inputPmIdNotInMgiList, CRT))
+        fpLogCur.write(CRT + 'Total: %s' % len(inputPmIdNotInMgiList))
     if len(inputPmIdMultiEgList):
-	fpLogCur.write(CRT + CRT + string.center(
+        fpLogCur.write(CRT + CRT + string.center(
             'PM IDs  Associated with > 15 egID in Input',60) + CRT)
-	fpLogCur.write('%-12s  %-20s%s' %
+        fpLogCur.write('%-12s  %-20s%s' %
             ('PM ID','EG IDs', CRT))
-	fpLogCur.write(string.join(inputPmIdMultiEgList, CRT))
-	fpLogCur.write(CRT + 'Total: %s' % len(inputPmIdMultiEgList))
+        fpLogCur.write(string.join(inputPmIdMultiEgList, CRT))
+        fpLogCur.write(CRT + 'Total: %s' % len(inputPmIdMultiEgList))
     if len(inputEgIdNotInMgiList):
-	fpLogCur.write(CRT + CRT + string.center(
+        fpLogCur.write(CRT + CRT + string.center(
             'EG IDs not in the Database',60) + CRT)
-	fpLogCur.write('%-12s  %-20s%s' %
+        fpLogCur.write('%-12s  %-20s%s' %
              ('EG ID','PM ID', CRT))
-	fpLogCur.write(string.join(inputEgIdNotInMgiList, CRT))
-	fpLogCur.write(CRT + 'Total: %s' % len(inputEgIdNotInMgiList))
+        fpLogCur.write(string.join(inputEgIdNotInMgiList, CRT))
+        fpLogCur.write(CRT + 'Total: %s' % len(inputEgIdNotInMgiList))
     if len(egIdMultiGenesList):
-	fpLogCur.write(CRT + CRT + string.center(
+        fpLogCur.write(CRT + CRT + string.center(
             'EG IDs associated with > 1 marker in the Database',60) + CRT)
-	fpLogCur.write('%-12s  %-20s  %-20s%s' %
+        fpLogCur.write('%-12s  %-20s  %-20s%s' %
              ('EG ID','PM ID', 'Markers', CRT))
-	fpLogCur.write(string.join(egIdMultiGenesList, CRT))
-	fpLogCur.write(CRT + 'Total: %s' % len(egIdMultiGenesList))
+        fpLogCur.write(string.join(egIdMultiGenesList, CRT))
+        fpLogCur.write(CRT + 'Total: %s' % len(egIdMultiGenesList))
     if len(dOrRStatusList):
-	fpLogCur.write(CRT + CRT + string.center(
+        fpLogCur.write(CRT + CRT + string.center(
             'References with isDiscard=true or Status=Rejected',60) + CRT)
         fpLogCur.write('%-12s  %-20s  %-20s%s' %
              ('Reference ID','isDiscard', 'Status', CRT))
@@ -521,11 +520,11 @@ def writeCuratorLog():
 #
 def closeFiles():
     if fpBcp:
-	fpBcp.close()
+        fpBcp.close()
     if fpLogCur:
-	fpLogCur.close()
+        fpLogCur.close()
     if fpLogDiag:
-	fpLogDiag.close()
+        fpLogDiag.close()
     return 0
 
 #
